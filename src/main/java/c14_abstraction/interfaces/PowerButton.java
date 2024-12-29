2a0f6f415433f4dc373fb42d00115df642bdc228
alswpddl <alswp0207@gmail.com>
2024-12-30 07:28:22 +0900

    feat: 상속 및 추상 및 인터페이스

---
+++ src/main/java/c14_abstraction/interfaces/PowerButton.java
@@ -0,0 +1,31 @@
+package c14_abstraction.interfaces;
+/*
+    아까전에 Button 클래스의 경우에는 인터페이스를 다중 상속 받았습니다.
+    즉 Predd, Up, Down이 가지고 있는 각각의 특징을 Button 클래스에 모아놨다고 볼 수 있습니다.
+    이를 PowerButton, ChannelUpButton, ChannelDownButton 등으로 나누어 각각 Button 클래스를
+    상속하도록 작성할 예정입니다.
+ */
+public class PowerButton extends Button {
+    private boolean status;
+
+    @Override
+    public void onDown() {
+        super.onDown();
+    }
+
+    @Override
+    public void onUp() {
+        super.onUp();
+    }
+
+    @Override
+    public void onPressed() {
+        if (status){            // 25 라인이 실행되기 위해서는 status = true여야만 하므로
+            status = false;     // if 문 내에는 전원을 끄는 명령어가 삽임돼야 함.
+            System.out.println("전원을 끕니다.");
+        } else {
+            status = true;
+            System.out.println("전원을 킵니다.");
+        }
+    }
+}
